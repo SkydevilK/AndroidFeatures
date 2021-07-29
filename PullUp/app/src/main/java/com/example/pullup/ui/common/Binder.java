df608bbdf6c493a52448852c3449de34c9121c9d
SkydevilK <skydevilk@gmail.com>
2021-07-29 21:50:21 +0900

    [Feat] Binder 생성

---
+++ PullUp/app/src/main/java/com/example/pullup/ui/common/Binder.java
@@ -0,0 +1,27 @@
+package com.example.pullup.ui.common;
+
+import android.widget.ImageView;
+
+import androidx.databinding.BindingAdapter;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class Binder {
+   @BindingAdapter({"android:src"})
+   public static void setImageViewResource(ImageView imageView, int resource) {
+       imageView.setImageResource(resource);
+   }
+   @BindingAdapter(value = {"itemList", "addItem"}, requireAll = false)
+    public static void bindItemList(RecyclerView recyclerView, ArrayList<MainItem> itemList, MainViewAdapter.OnAddItemListener addItemListener) {
+       MainViewAdapter adapter;
+       if(recyclerView.getAdapter() == null) {
+           adapter = new MainViewAdapter();
+           recyclerView.setAdapter(adapter);
+       } else {
+           adapter = (MainViewAdapter) recyclerView.getAdapter();
+       }
+       adapter.setItem(itemList);
+       adapter.setOnAddItemListener(addItemListener);
+   }
+}
